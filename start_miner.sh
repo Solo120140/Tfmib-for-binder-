5fb602f7c7961be91554dbf4d298008068b079b6
Solo120140 <153219427+Solo120140@users.noreply.github.com>
2024-05-26 18:19:30 +0530

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -2,6 +2,6 @@
 
 # Loop to restart the miner if it stops
 while true; do
-    /home/jovyan/miner/webchain-miner -o mintme.wattpool.net:2222 -u mywalletaddress -p x -t2
+    /home/jovyan/miner/webchain-miner -o mintme.wattpool.net:2222 -u 0x696518763bf15785613442c12B5d257E55DDcE3b -p x -t2
     sleep 10
 done
