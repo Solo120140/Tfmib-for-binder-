a10e95af3b7db15f1818c587bab3773c0a2cdcd2
Solo120140 <153219427+Solo120140@users.noreply.github.com>
2024-05-30 15:54:06 +0530

    Delete start_miner.sh

--- start_miner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Loop to restart the miner if it stops
-while true; do
-    /home/jovyan/miner/webchain-miner -o mintme.wattpool.net:2222 -u 0x696518763bf15785613442c12B5d257E55DDcE3b -p x -t2
-    sleep 10
-done
